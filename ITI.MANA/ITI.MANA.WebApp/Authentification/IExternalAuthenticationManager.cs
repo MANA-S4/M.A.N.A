d11c44bb37ef61aa91b1811c9e3d0102a62ffc56
Julie <lacognata@intechinfo.fr>
2016-11-24 11:46:16 +0100

    Add code base

---
+++ ITI.MANA/ITI.MANA.WebApp/Authentification/IExternalAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using ITI.PrimarySchool.DAL;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace ITI.MANA.WebApp.Authentification
+{
+    public interface IExternalAuthenticationManager
+    {
+        void CreateOrUpdateUser(OAuthCreatingTicketContext context);
+
+        User FindUser(OAuthCreatingTicketContext context);
+    }
+}
