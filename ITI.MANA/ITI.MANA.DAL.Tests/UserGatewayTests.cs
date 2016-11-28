4173fe6e98f00bcb11f771af84cad8c5c4ded40b
Julie <lacognata@intechinfo.fr>
2016-11-28 11:16:42 +0100

    Add NunitLite
    Add class SimpleTests contained ShouldPass and ShouldFail
    Add tests on project Sample to try the runners

--- ITI.MANA/ITI.MANA.DAL.Tests/UserGatewayTests.cs
+++ ITI.MANA/ITI.MANA.DAL.Tests/UserGatewayTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace ITI.MANA.DAL.Tests
 {
