cca4cd55816b171cca164721ffda38c14e83f738
Julie <lacognata@intechinfo.fr>
2016-11-29 09:35:58 +0100

    Add some corrections

--- ITI.MANA/ITI.MANA.WebApp2/Startup.cs
+++ ITI.MANA/ITI.MANA.WebApp2/Startup.cs
@@ -2,11 +2,9 @@
 using ITI.MANA.DAL;
 using ITI.MANA.WebApp.Authentification;
 using ITI.MANA.WebApp.Services;
-using Microsoft.AspNetCore.Authentification.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependancyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
