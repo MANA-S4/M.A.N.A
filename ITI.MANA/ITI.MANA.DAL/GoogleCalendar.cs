699158321e49554d234d7de4178429fe63ca517f
Julie <lacognata@intechinfo.fr>
2016-12-07 14:53:15 +0100

    Add GoogleCalendar

---
+++ ITI.MANA/ITI.MANA.DAL/GoogleCalendar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace ITI.MANA.DAL
+{
+    public class GoogleCalendar
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+
+
+        // Create Google Calendar API service.
+        /*var service = new CalendarService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });*/
+    }
+}
