21c0b77e2060bc33fadc182bced2aa7cedcbd01c
Yacine <chergui@intechinfo.fr>
2016-12-08 11:52:04 +0100

    Create GoogleCalendarGateway Add some summary Create first test

---
+++ ITI.MANA/ITI.MANA.DAL/GoogleCalendarGateway.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace ITI.MANA.DAL
+{
+    public class GoogleCalendarGateway
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+        static EventsResource.ListRequest request = service.Events.List("primary");
+
+        static UserCredential credential;
+        Events events;
+
+        static CalendarService service = new CalendarService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
+
+        /// <summary>
+        /// Defines the google request parameters.
+        /// </summary>
+        public void DefineGoogleRequestParameters()
+        {
+            request.SingleEvents = true;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+        }
+
+        /// <summary>
+        /// Gets the list events.
+        /// </summary>
+        /// <returns></returns>
+        public Events GetListEvents()
+        {
+            return events = request.Execute();
+        }
+    }
+}
