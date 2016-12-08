6546a29f91231ff6224ab8f12efcfdb88397eee2
Yacine <chergui@intechinfo.fr>
2016-12-08 09:38:53 +0100

    update gitignore
    make base of GoogleCalendar.cs

--- ITI.MANA/ITI.MANA.DAL/GoogleCalendar.cs
+++ ITI.MANA/ITI.MANA.DAL/GoogleCalendar.cs
@@ -17,14 +17,51 @@ namespace ITI.MANA.DAL
     {
         static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
         static string ApplicationName = "Google Calendar API .NET Quickstart";
+        static EventsResource.ListRequest request = service.Events.List("primary");
 
+        static UserCredential credential;
 
-
-        // Create Google Calendar API service.
-        /*var service = new CalendarService(new BaseClientService.Initializer()
+        static CalendarService service = new CalendarService(new BaseClientService.Initializer()
         {
             HttpClientInitializer = credential,
             ApplicationName = ApplicationName,
-        });*/
+        });
+
+
+        // Define parameters of request.
+        public void DefineGoogleRequestParameters()
+        {
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+        }
+
+
+        // List events.
+        public void GetListEvents()
+        {
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            Console.Read();
+        }
+
     }
 }
