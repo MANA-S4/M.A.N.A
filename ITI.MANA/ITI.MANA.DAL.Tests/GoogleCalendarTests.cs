b53e76b3ca72a1dec18a1cc1aa13391045965ab9
Yacine <chergui@intechinfo.fr>
2016-12-27 12:27:56 +0100

    Add test for the googleGateway

--- ITI.MANA/ITI.MANA.DAL.Tests/GoogleCalendarTests.cs
+++ ITI.MANA/ITI.MANA.DAL.Tests/GoogleCalendarTests.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Google.Apis.Auth.OAuth2.Responses;
+
 
 namespace ITI.MANA.DAL.Tests
 {
     [TestFixture]
     public class GoogleCalendarTests
     {
+        public GoogleCalendarGateway sut = new GoogleCalendarGateway(TestHelpers.ConnectionString);
+
         [Test]
         public void GetTokenResponseReturnValue()
         {
-            
+            TokenResponse token = sut.GetResponseToken(13);
+            Assert.That(token != null);
         }
     }
 }
