4c4b861f2f3fa2533d4acdecd65752e3cb256364
Julie <lacognata@intechinfo.fr>
2017-01-03 14:30:34 +0100

    Reinit TaskList.vue

--- ITI.MANA/ITI.MANA.WebApp2/App/mana/src/components/Tasks/TaskList.vue
+++ ITI.MANA/ITI.MANA.WebApp2/App/mana/src/components/Tasks/TaskList.vue
@@ -9,61 +9,27 @@
                 <router-link class="btn btn-primary" :to="`tasks/create`"><i class="glyphicon glyphicon-plus"></i> Ajouter une tâche</router-link>
             </div>
 
-        </div>
-            <div class="card is-fullwidth">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        Component
-                    </p>
-                    <a class="card-header-icon">
-                        <i class="fa fa-angle-down"></i>
-                    </a>
-                </header>
-
-                <div class="card-content">
-                    <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    <br>
-                    <small>11:09 PM - 1 Jan 2016</small>
-                </div>
-            </div>
-                <footer class="card-footer">
-                <a class="card-footer-item">Save</a>
-                <a class="card-footer-item">Edit</a>
-                <a class="card-footer-item">Delete</a>
-                </footer>
-        </div>
-
-
-        <!--<table class="table table-striped table-hover table-bordered">
+        <table class="table table-striped table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>ContactId</th>
-                    <th>Email</th>
-                    <th>RelationType</th>
+                    <th>TaskId</th>
+                    <th>ToDo</th>
+                    <th>User</th>
                     <th>Options</th>       
                 </tr>
-            </thead>-->
+            </thead>
 
             <tbody>
-                <!--<tr v-if="contactList.length == 0">
-                    <td colspan="5" class="text-center">Il n'y a actuellement aucun contact.</td>
-                </tr>-->
-
-                <!--<tr v-for="i of list">
-                    <td>{{ i.contactId }}</td>
-                    <td>{{ i.email }}</td>
-                    <td>{{ i.relationType }}</td>
+                <tr v-for="i of list">
+                    <td>{{ i.taskId }}</td>
+                    <td>{{ i.toDo }}</td>
+                    <td>{{ i.user }}</td>
                     <td>
-                        <router-link :to="`contacts/edit/${i.contactId}`"><i class="glyphicon glyphicon-pencil"></i></router-link>
-                        <a href="#" @click="deleteContact(i.contactId)"><i class="glyphicon glyphicon-remove"></i></a>          
+                        <router-link :to="`tasks/edit/${i.taskId}`"><i class="glyphicon glyphicon-pencil"></i></router-link>
+                        <a href="#" @click="deleteTask(i.taskId)"><i class="glyphicon glyphicon-remove"></i></a>          
                     </td>
                 </tr>
 
-                <tr>
-                    <a href="#" @click="previousList(i.contactId)"><i class="glyphicon glyphicon-menu-left"></i></a> 
-                    <a href="#" @click="nextList(i.contactId)"><i class="glyphicon glyphicon-menu-right"></i></a>
-                </tr>-->
             </tbody>
         </table>
     </div>
@@ -83,29 +49,29 @@
         //-------------------------------
 
         created() {
-            this.refreshContactList();
+            this.refreshTaskList();
         },
 
         computed: {
-            ...mapGetters(['contactList']),
+            ...mapGetters(['taskList']),
 
             /// ---------------------- Question 4 -------------------------------------
             list: function() {
-                let contact =  [];
+                let task =  [];
                 let i = 0;
                 
-                for(i = 0; i < this.contactList.length; i++) {
-                    if (this.contactList[i].email.includes(this.search)) {
-                        contact.push(this.contactList[i]);
+                for(i = 0; i < this.taskList.length; i++) {
+                    if (this.taskList[i].toDo.includes(this.search)) {
+                        task.push(this.taskList[i]);
                     }
                 }
-                return contact;
+                return task;
             }
             /// ------------------------------------------------------------------------   
         },
 
         methods: {
-            ...mapActions(['refreshContactList' ,'deleteContact'])
+            ...mapActions(['refreshTaskList' ,'deleteTask'])
         }
     }
 </script>
