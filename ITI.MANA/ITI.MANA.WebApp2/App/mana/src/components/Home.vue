d63b2967f8974396c169eae27a311d8df0e0673c
Julie <lacognata@intechinfo.fr>
2017-01-09 17:16:27 +0100

    Add interface into web site
    Have to continue

--- ITI.MANA/ITI.MANA.WebApp2/App/mana/src/components/Home.vue
+++ ITI.MANA/ITI.MANA.WebApp2/App/mana/src/components/Home.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="jumbotron">
-        <h1>Bienvenue sur M.A.N.A</h1>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
 
-        <p>
-            VIVE LES MOUTONS !
-        </p>
+          <div class="inner cover">
+            <h1 class="cover-heading">M.A.N.A</h1>
+            <p class="lead">Notre application vous aidera à organiser votre vie familiale ou entre colocataire. Vous pourrez ajouter des contacts, gérer vos calendriers (et inviter votre famille ou vos coloc' à des évènements). De plus vous pourrez ajouter des tâches pour savoir ce qu'il vous reste à faire.</p>
+          </div>
 
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Application réalisée par Julie L et Yacine C - IN'TECH - Semestre 4 IL.</p>
+            </div>
+          </div>
+
+      </div>
     </div>
 </template>
 
@@ -13,7 +21,6 @@
     export default {
 
     }
-
 </script>
 
 <style lang="less">
